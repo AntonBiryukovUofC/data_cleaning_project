bf5056e61c058709e62ed373a95e1032d33f3c76
anton.biryukov@ucalgary.ca <anton.biryukov@ucalgary.ca>
2016-03-05 01:04:19 -0500

    added runAnalysis.R

---
+++ CookBook.md
@@ -0,0 +1,16 @@
+# Cookbook for the final project in Getting and Cleaning Data Course
+
+Here I describe the succession of steps leading to the clean dataset .
+The workflow implemented in run_analysis.R is as follows:
+1. Load the activity label(char) -- id(int) map (activity_label.txt)
+2. Create a temporary function "map_activity" that assigns the activity label as a char to the given number if found in the id-s list
+3. Read the feature list with their ids, corresponding to a column number in the training/test dataset.
+4. Select only those features that describe SD or mean of data measurements for measured quantities.
+5. Read the training dataset, keep only the columns that correspond to the selected features.
+6. Read the training activity and subject labels ("train_set_label" and "train_set_subject"), put labels on the activity and add both columns to the train_set (subjectID and activityDesc, respectively).
+
+7. Repeat 5-6 applied to the test set data.
+8. Combine "test_set" and "train_set" into "total_set".
+9. Group the total_set by unique values in "subjectID" and "activityDesc"
+10. Add n_obs column showing the number of observations in each column
+11. Using summarize_each function from dplyr I find mean values for each of the quantities for each of the groups.
--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,70 @@
+require(plyr)
 library(dplyr)
 library(lubridate)
 library(tidyr)
-train_set <- read.table("../UCI HAR Dataset/train/x_train_small.txt")
-train_set_label <- read.csv("../UCI HAR Dataset/train/y_train.txt")[1:nrow(train_set),]
 
-test_set <- read.table("../UCI HAR Dataset/test/x_test_small.txt")
-test_set_label <- read.csv("../UCI HAR Dataset/test/y_test.txt")[1:nrow(test_set),]
+# Reading the activity String-Integer map:
+activity_list <- tbl_df(read.table("../UCI HAR Dataset/activity_labels.txt",
+                                   col.names=c("activityID","activityTitle"), stringsAsFactors=FALSE))
+# Make a function that converts the ID of activity to its label
+map_activity <- function(x) {activity_list$activityTitle[x == activity_list$activityID] }
 
+
+# Reading the feature list and grep the -mean() and -std() only;
+# We find the indices of the columns, so that we read only those columns
+feature_list <- tbl_df(read.table("../UCI HAR Dataset/features.txt",col.names=c("featureID","featureTitle")))
+inds_feature <- grep("*-(mean|std)\\(",feature_list$featureTitle)
+ ########### ############### ################# ################## ##################### #################### ##
+# Reading training set, only the columns with mean or std
+#train_set <- tbl_df(read.table("../UCI HAR Dataset/train/x_train_small.txt")[,inds_feature])
+train_set <- tbl_df(read.table("../UCI HAR Dataset/train/X_train.txt")[,inds_feature])
+# Set the column names to the measured values: delete dashes and brackets, sub "_" instead
+colnames(train_set) <- gsub("-|\\(|\\)","_",feature_list$featureTitle[inds_feature])
+# Check for double "__"
+colnames(train_set) <- gsub("__","",colnames(train_set))
+# Read the training set and label values; the nrow(train_set) argument is used for debugging
+# Debugging was done on a small subsets of test and train sets
+train_set_label <- read.table("../UCI HAR Dataset/train/y_train.txt")
+train_set_subject <- read.table("../UCI HAR Dataset/train/subject_train.txt")
+# Add the activity label and subjectID columns to the dataset, bring them to the left
+train_set<- train_set %>% 
+  mutate(activityDesc = sapply(train_set_label$V1,map_activity)) %>%
+  mutate(subjectID = train_set_subject$V1) %>%
+  select(activityDesc,subjectID,everything())
+print(" Training set has been processed successfully, moving on to test set...")
+########### ############### ################# ################## ##################### #################### ##
+# Reading test set, only columns with mean and std values
+#set.seed(120)
+#tmp_ids = sample(1:500,40);
+#test_set <- tbl_df(read.table("../UCI HAR Dataset/test/X_test.txt")[tmp_ids,inds_feature])
+test_set <- tbl_df(read.table("../UCI HAR Dataset/test/X_test.txt")[,inds_feature])
+# Set the column names to the measured values, delete unnecessary dashes and brackets
+colnames(test_set) <- gsub("-|\\(|\\)","_",feature_list$featureTitle[inds_feature])
+colnames(test_set) <- gsub("__","",colnames(test_set))
+
+test_set_label <- read.table("../UCI HAR Dataset/test/y_test.txt")
+test_set_subject <- read.table("../UCI HAR Dataset/test/subject_test.txt")
+
+#test_set_label <- read.csv("../UCI HAR Dataset/test/y_test.txt")[tmp_ids,]
+#test_set_subject <- read.csv("../UCI HAR Dataset/test/subject_test.txt")[tmp_ids,]
+# Add the activity label and subjectID columns to the dataset, bring them to the left
+test_set<- test_set %>% 
+  mutate(activityDesc = sapply(test_set_label$V1,map_activity)) %>%
+  mutate(subjectID = test_set_subject$V1) %>%
+  select(activityDesc,subjectID,everything())
+
+print(" Test set has been processed successfully, moving on to merging...")
+
+########### ############### ################# ################## ##################### #################### ##
+# Merge datasets by binding one to the bottom of the other:
+total_set <- rbind(train_set,test_set)
+#rm("test_set","train_set","train_set_subject","train_set_label","test_set_subject","test_set_label")
+# Group by activity and the subject ID
+avg_per_group <-total_set %>% group_by(subjectID,activityDesc) %>%
+   mutate(n_obs = n())  %>%  # Calculate how many observations are in each group
+   select(n_obs,everything())  %>% # Move that column to the left for clarity
+   summarise_each(funs(mean))  %>% # Average each column for each group
+   arrange(subjectID)# Sort by the subjectID for clarity
+
+write.csv(avg_per_group, file = "./tidy_ds_average_values.csv")
+print(" Tidy dataset has been written on HDD...")
